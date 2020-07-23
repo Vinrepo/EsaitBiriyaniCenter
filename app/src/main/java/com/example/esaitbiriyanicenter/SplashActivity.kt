7a276c491b894a96c35a17b84ccb7de7d3f204a6
Vinoth <vinraja100@gmail.com>
2020-07-24 03:10:53 +0530

    added splash screen

---
+++ app/src/main/java/com/example/esaitbiriyanicenter/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.esaitbiriyanicenter
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    var handler: Handler? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        handler = Handler()
+        handler!!.postDelayed(Runnable {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+}
