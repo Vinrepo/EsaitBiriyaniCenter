02a6855c6e4ce0b64cdf77664971f21200d21f0f
Akash Babu <babushanthi207@gmail.com>
2020-08-08 22:06:15 +0530

    add emmmnt line

--- app/src/main/java/com/example/esaitbiriyanicenter/MainActivity.kt
+++ app/src/main/java/com/example/esaitbiriyanicenter/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
-
+//svsdvsdb
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
