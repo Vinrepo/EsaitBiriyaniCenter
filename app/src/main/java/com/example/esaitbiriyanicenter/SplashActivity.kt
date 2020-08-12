e97ef32ead9483791e490cb547a2b195f30ae530
Vinoth <vinraja100@gmail.com>
2020-08-12 16:33:56 +0530

    added splash screen

--- app/src/main/java/com/example/esaitbiriyanicenter/SplashActivity.kt
+++ app/src/main/java/com/example/esaitbiriyanicenter/SplashActivity.kt
@@ -3,7 +3,14 @@ package com.restaurant.esaitbiriyanicenter
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.esaitbiriyanicenter.Availability
+import com.restaurant.esaitbiriyanicenter.api.APIClient
+import com.restaurant.esaitbiriyanicenter.api.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class SplashActivity : AppCompatActivity() {
@@ -11,12 +18,26 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
-        handler = Handler()
+        val call = APIClient.client.create(ApiInterface::class.java).getBooks()
+        call.enqueue(object : Callback<Availability> {
+            override fun onResponse(call: Call<Availability>, response: Response<Availability>) {
+                Log.d("Success!", response.toString())
+                var text = response.body()
+                var bookList = text?.values
+            }
+
+            override fun onFailure(call: Call<Availability>, t: Throwable)                  {
+                Log.e("Failed Query :(", t.toString())
+
+            }
+        })
+    }
+        /*handler = Handler()
         handler!!.postDelayed(Runnable {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 2000)*/
     }
-}
+
 
