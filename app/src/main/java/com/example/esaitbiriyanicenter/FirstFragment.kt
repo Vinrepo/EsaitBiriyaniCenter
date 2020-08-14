ded9dadea81bcca53eab8ea6f2ed7301ef1e7b0c
Vinoth <vinraja100@gmail.com>
2020-08-14 21:18:13 +0530

    added splash screen

--- app/src/main/java/com/example/esaitbiriyanicenter/FirstFragment.kt
+++ app/src/main/java/com/example/esaitbiriyanicenter/FirstFragment.kt
@@ -1,14 +1,23 @@
 package com.restaurant.esaitbiriyanicenter
 
+import android.Manifest
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -20,15 +29,29 @@ import com.android.volley.RetryPolicy
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_first.toolbar
+import kotlinx.android.synthetic.main.fragment_second.*
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import org.json.JSONException
 import org.json.JSONObject
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+import kotlin.math.cos
 
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    val PERMISSION_ID = 1010
+    var distance = 0.0;
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,6 +65,14 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState);
         toolbar.setTitle("Esait Biriyani center");
         (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
+        /*** Distance calculation ****/
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient( requireContext())
+        Log.d("Debug:",CheckPermission().toString())
+        Log.d("Debug:",isLocationEnabled().toString())
+        RequestPermission()
+        getLastLocation()
+        Toast.makeText(context,distance.toString(),Toast.LENGTH_SHORT).show()
+        /*** Distance calculation ****/
 
         val childList1: MutableList<Child> = ArrayList()
         childList1.add(Child("Mutton Biriyani  ₹270/pack"));
@@ -213,4 +244,142 @@ class FirstFragment : Fragment() {
             e.printStackTrace()
         }
     }
+
+    fun RequestPermission(){
+        //this function will allows us to tell the user to requesut the necessary permsiion if they are not garented
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+    fun isLocationEnabled():Boolean{
+        //this function will return to us the state of the location service
+        //if the gps or the network provider is enabled then it will return true otherwise it will return false
+
+        var locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+    fun getLastLocation(){
+
+        if(CheckPermission()){
+            if(isLocationEnabled()){
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                   return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task->
+                    var location: Location? = task.result
+                    if(location == null){
+                        Toast.makeText(context,"Please new",Toast.LENGTH_SHORT).show()
+                        NewLocationData()
+                    }else{
+                        Toast.makeText(context,"ruunned",Toast.LENGTH_SHORT).show()
+                        val long1 = 80.1266854 / 57.29577951
+                        val lat1 = 12.9231058 / 57.29577951
+                        val long2 = location.longitude / 57.29577951
+                        val lat2 = location.latitude / 57.29577951
+
+                        val Distance = 3963.0 * StrictMath.acos(
+                            (StrictMath.sin(lat1) * StrictMath.sin(
+                                lat2
+                            )) + cos(lat1) * cos(lat2) * cos(long2 - long1)
+                        )
+                       val fin = Distance*1.60934
+                        distance = fin * 1.6;
+                        //Log.d("Debug:" ,"Your Location:"+ location.longitude)
+                        //Toast.makeText(context,"lon"+location.longitude,Toast.LENGTH_SHORT).show()
+                        //editTextTextEmailName.text = "You Current Location is : Long: "+ location.longitude + " , Lat: " + location.latitude + "\n"
+
+                    }
+
+                }
+            }else{
+                Toast.makeText(context,"Please Turn on Your device Location",Toast.LENGTH_SHORT).show()
+            }
+
+        }else{
+            RequestPermission()
+        }
+
+    }
+    fun NewLocationData(){
+        var locationRequest =  LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 1
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            locationRequest,locationCallback, Looper.myLooper()
+        )
+
+    }
+
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            var lastLocation: Location = locationResult.lastLocation
+            Log.d("Debug:","your last last location: "+ lastLocation.longitude.toString())
+            //textView.text = "You Last Location is : Long: "+ lastLocation.longitude + " , Lat: " + lastLocation.latitude + "\n" + getCityName(lastLocation.latitude,lastLocation.longitude)
+        }
+    }
+    private fun getCityName(lat: Double,long: Double):String{
+        var cityName:String = ""
+        var countryName = ""
+        var geoCoder = Geocoder(requireContext(), Locale.getDefault())
+        var Adress = geoCoder.getFromLocation(lat,long,3)
+
+        cityName = Adress.get(0).locality
+        countryName = Adress.get(0).countryName
+        Log.d("Debug:","Your City: " + cityName + " ; your Country " + countryName)
+        return cityName
+    }
+
+    fun CheckPermission():Boolean{
+        //this function will return a boolean
+        //true: if we have permission
+        //false if not
+        if(
+            ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ){
+            return true
+        }
+
+        return false
+
+    }
+
 }
--- app/src/main/java/com/example/esaitbiriyanicenter/SecondFragment.kt
+++ app/src/main/java/com/example/esaitbiriyanicenter/SecondFragment.kt
@@ -1,56 +1,31 @@
 package com.restaurant.esaitbiriyanicenter
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.location.Geocoder
-import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
-import android.provider.ContactsContract
-import android.provider.Settings
-import com.google.android.gms.location.*
-import kotlinx.android.synthetic.main.activity_main.*
-import java.security.Permission
-import java.security.Provider
-import java.util.*
+
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
 import android.net.Uri
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.esaitbiriyanicenter.DatabaseHandler
 import com.example.esaitbiriyanicenter.EmpModelClass
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_second.*
 import kotlinx.android.synthetic.main.fragment_second.toolbar
 import java.lang.Exception
-import java.lang.StrictMath.*
-import kotlin.math.cos
-
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
     var i = "100"
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    lateinit var locationRequest: LocationRequest
-    val PERMISSION_ID = 1010
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -64,12 +39,10 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState);
         super.onCreate(savedInstanceState)
+
+
         var c = 0
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient( requireContext())
-        Log.d("Debug:",CheckPermission().toString())
-        Log.d("Debug:",isLocationEnabled().toString())
-        RequestPermission()
-        getLastLocation()
+
         //fun viewRecord(context: Context?){
         //creating the instance of DatabaseHandler class
         val databaseHandler: DatabaseHandler= DatabaseHandler(context)
@@ -181,133 +154,7 @@ class SecondFragment : Fragment() {
         }
 
     }
-    fun CheckPermission():Boolean{
-        //this function will return a boolean
-        //true: if we have permission
-        //false if not
-        if(
-            ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-        ){
-            return true
-        }
 
-        return false
-
-    }
-    fun RequestPermission(){
-        //this function will allows us to tell the user to requesut the necessary permsiion if they are not garented
-        ActivityCompat.requestPermissions(
-            requireActivity(),
-            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
-            PERMISSION_ID
-        )
-    }
-    fun isLocationEnabled():Boolean{
-        //this function will return to us the state of the location service
-        //if the gps or the network provider is enabled then it will return true otherwise it will return false
-
-        var locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-    }
-    fun getLastLocation(){
-        if(CheckPermission()){
-            if(isLocationEnabled()){
-                if (ActivityCompat.checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return
-                }
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task->
-                    var location: Location? = task.result
-                    if(location == null){
-                        Toast.makeText(context,"Please new",Toast.LENGTH_SHORT).show()
-                        NewLocationData()
-                    }else{
-                        Toast.makeText(context,"ruunned",Toast.LENGTH_SHORT).show()
-                        val long1 = 80.1180644 / 57.29577951
-                        val lat1 = 12.9233093 / 57.29577951
-                        val long2 = location.longitude / 57.29577951
-                        val lat2 = location.latitude / 57.29577951
-
-                        val Distance = 3963.0 * acos((sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(long2-long1))
-                        val fin = Distance*1.60934
-
-                        //Log.d("Debug:" ,"Your Location:"+ location.longitude)
-                        //Toast.makeText(context,"lon"+location.longitude,Toast.LENGTH_SHORT).show()
-                        //editTextTextEmailName.text = "You Current Location is : Long: "+ location.longitude + " , Lat: " + location.latitude + "\n"
-                        editTextTextEmailName.setText(fin.toString());
-                        editTextPhone.setText(getCityName(location.latitude,location.longitude));
-                    }
-                }
-            }else{
-                Toast.makeText(context,"Please Turn on Your device Location",Toast.LENGTH_SHORT).show()
-            }
-        }else{
-            RequestPermission()
-        }
-    }
-    fun NewLocationData(){
-        var locationRequest =  LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 0
-        locationRequest.fastestInterval = 0
-        locationRequest.numUpdates = 1
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        fusedLocationProviderClient!!.requestLocationUpdates(
-            locationRequest,locationCallback, Looper.myLooper()
-        )
-
-    }
-
-
-    private val locationCallback = object : LocationCallback(){
-        override fun onLocationResult(locationResult: LocationResult) {
-            var lastLocation: Location = locationResult.lastLocation
-            Log.d("Debug:","your last last location: "+ lastLocation.longitude.toString())
-            //textView.text = "You Last Location is : Long: "+ lastLocation.longitude + " , Lat: " + lastLocation.latitude + "\n" + getCityName(lastLocation.latitude,lastLocation.longitude)
-        }
-    }
-    private fun getCityName(lat: Double,long: Double):String{
-        var cityName:String = ""
-        var countryName = ""
-        var geoCoder = Geocoder(requireContext(), Locale.getDefault())
-        var Adress = geoCoder.getFromLocation(lat,long,3)
-
-        cityName = Adress.get(0).locality
-        countryName = Adress.get(0).countryName
-        Log.d("Debug:","Your City: " + cityName + " ; your Country " + countryName)
-        return cityName
-    }
 
     fun updateRecord(context: Context?){
         //val dialogBuilder = AlertDialog.Builder(this)
